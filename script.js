cd5275ac3466f042bde0812476b03d7b4a1a4ee7
Thomas-474 <teg2004@icloud.com>
2021-08-27 10:39:07 -0400

    Random Number

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 var rangeStart;
 var rangeEnd;
+var randomNumber;
 
 function AutoGuess(){
     rangeStart = document.getElementById('startNumb').value;
     rangeEnd = document.getElementById('endNumb').value;
+    randomNumber = parseInt(Math.round(Math.random() * (rangeEnd - rangeStart) + rangeStart)) + parseInt(rangeStart);
 
     // temp
         console.log(rangeStart);
         console.log(rangeEnd);
+        console.log(randomNumber);
     //
 }
