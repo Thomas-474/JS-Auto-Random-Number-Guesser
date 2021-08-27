e2453198908c0002085b6c73ff03a98d7f3924a2
Thomas-474 <teg2004@icloud.com>
2021-08-27 09:43:29 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var rangeStart;
+var rangeEnd;
+
+function AutoGuess(){
+    rangeStart = document.getElementById('startNumb').value;
+    rangeEnd = document.getElementById('endNumb').value;
+
+    // temp
+        console.log(rangeStart);
+        console.log(rangeEnd);
+    //
+}
